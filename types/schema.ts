9ef08037c057967183b83e4b9b9124ac3f37821f
Belrick Stephane <73307766+latioms@users.noreply.github.com>
2025-04-05 08:17:10 +0100

    feat(schema): Add initial type definitions for Project, Contact, ContactGroup, Phonebook, Campaign, and Message

---
+++ types/schema.ts
@@ -0,0 +1,40 @@
+
+export type Project = {
+    id: string;
+    sender_name: string;
+    account_id: string;
+}
+
+export type Contact = {
+    contact_id: string;
+    phonebook_id: string;
+    first_name: string;
+    last_name: string;
+    phone: string;
+}
+
+export type ContactGroup = {
+    group_id: string;
+    group_name: string;
+}
+
+export type Phonebook = {
+    phonebook_id: string;
+    project_id: string;
+}
+
+export type Campaign = {
+    campaign_id: string;
+    name: string;
+    project_id: string;
+    scheduled_time: Date;
+    status: string;
+    messages: Message[]
+}
+
+export type Message = {
+    message_id: string;
+    delivered_status: string;
+    content: string;
+    sent_time: Date;
+}
