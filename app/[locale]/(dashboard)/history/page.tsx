77e4cdaea7ef51ecceff3da24b367ad731e6dab8
Belrick Stephane <73307766+latioms@users.noreply.github.com>
2025-05-09 16:59:34 +0100

    fix: Adjust margin for title in history page and update message description in MessagesHistoryTable

--- app/[locale]/(dashboard)/history/page.tsx
+++ app/[locale]/(dashboard)/history/page.tsx
@@ -4,7 +4,7 @@ import { MessagesHistoryTable } from '@/components/history/messages-history-tabl
 export default function page() {
   return (
     <section className='py-15'>
-      <h1 className='text-2xl font-semibold mb-5'>Historique</h1>
+      <h1 className='text-2xl font-semibold m-5'>Historique</h1>
       <MessagesHistoryTable />
     </section>
   )
