04f8d3b24a4ebcdfaf47f9a0e570787e603a7fde
Belrick Stephane <73307766+latioms@users.noreply.github.com>
2025-05-09 22:54:09 +0100

    feat: Implement multilingual support for campaigns and history pages; update UI components to use dictionary for text

--- app/[locale]/(dashboard)/history/page.tsx
+++ app/[locale]/(dashboard)/history/page.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { MessagesHistoryTable } from '@/components/history/messages-history-table'
+import { getDictionary } from '../../dictionaries'
+import { getLang } from '@/lib/lang'
 
-export default function page() {
+export default async function HistoryPage() {
+  const lang = await getLang();
+  const t = await getDictionary(lang);
+
   return (
     <section className='py-15'>
-      <h1 className='text-2xl font-semibold m-5'>Historique</h1>
-      <MessagesHistoryTable />
+      <h1 className='text-2xl font-semibold m-5'>{t.history.title}</h1>
+      <MessagesHistoryTable dictionary={t.history} />
     </section>
   )
 }
--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,7 +1,7 @@
 import { getLang } from '@/lib/lang';
 import { getDictionary } from './dictionaries';
 
-export  default async function Page() { 
+export default async function Page() { 
     
     const lang = await getLang();
     const t = await getDictionary(lang);
