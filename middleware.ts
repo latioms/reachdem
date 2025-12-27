5df988aa26be6097f74a2b84f768690442f8b394
Belrick Stephane <latioms@gmail.com>
2025-12-27 17:26:22 +0100

    chore: remove deprecated middleware.ts file as it is replaced by proxy.ts

--- middleware.ts
+++
@@ -1,6 +0,0 @@
-// Deprecated: middleware.ts is replaced by proxy.ts
-// See: https://nextjs.org/docs/messages/middleware-to-proxy
-// This file intentionally no longer exports `middleware` to avoid the
-// deprecated middleware file convention being picked up by Next.js.
-
-export {};
